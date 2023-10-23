703a1ad1a1d169e804d146c83de7629b51915127
Leandro Palmeira <leandropalmeira@hotmail.com>
2023-10-23 20:55:23 -0300

    Update index.js

--- frontend/src/pages/TicketsCustom/index.js
+++ frontend/src/pages/TicketsCustom/index.js
@@ -13,7 +13,7 @@ const useStyles = makeStyles(theme => ({
 	chatContainer: {
 		flex: 1,
 		// backgroundColor: "#eee",
-		padding: theme.spacing(4),
+		padding: theme.spacing(1),
 		height: `calc(100% - 48px)`,
 		overflowY: "hidden",
 	},
